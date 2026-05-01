e6dddf795befb340e0f840c9491bffc010e2fe21
Lana <grudininasv2018@gmail.com>
2026-05-01 10:00:23 +0300

    monorepo: backend+webapp, scripts

---
+++ node_modules/@miniaturenick/backend/src/trpc.ts
@@ -0,0 +1,45 @@
+import { initTRPC } from "@trpc/server";
+
+const cards = [
+  {
+    nick: "card-1",
+    historical_period: "period",
+    author: "nickname",
+    title: "Миниатюра-1",
+    description: "1",
+  },
+  {
+    nick: "card-2",
+    historical_period: "period",
+    author: "nickname",
+    title: "Миниатюра-2",
+    description: "2",
+  },
+  {
+    nick: "card-3",
+    historical_period: "period",
+    author: "nickname",
+    title: "Миниатюра-3",
+    description: "3",
+  },
+  {
+    nick: "card-4",
+    historical_period: "period",
+    author: "nickname",
+    title: "Миниатюра-4",
+    description: "4",
+  },
+];
+
+// инициализируем
+const trpc = initTRPC.create();
+
+// создаем роутер,который возвращает инфу с backend
+export const trpcRouter = trpc.router({
+  getCards: trpc.procedure.query(() => {
+    return { cards };
+  }),
+});
+
+// типы автоматически “протекают” на клиент, на frontend
+export type TrpcRouter = typeof trpcRouter;
---
+++ webapp/src/lib/trpc.tsx
@@ -0,0 +1,33 @@
+import type { TrpcRouter } from "@miniaturenick/backend";
+import { createTRPCReact } from "@trpc/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+
+// доступ к клиентской trpc, <передача типов из бэкенда>
+export const trpc = createTRPCReact<TrpcRouter>();
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+const trpcClient = trpc.createClient({
+  links: [
+    httpBatchLink({
+      url: "http://localhost:3000/trpc",
+    }),
+  ],
+});
+
+// будет провайдить в react приложение
+export const TrpcProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
+  );
+};
