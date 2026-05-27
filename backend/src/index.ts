dbd19b9e6af5bc532e6e26e50213e113c2aee467
Lana <grudininasv2018@gmail.com>
2026-05-27 14:12:39 +0300

    prisma + trpc

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,31 +24,4 @@ const bootstrap = async () => {
 };
 
 void bootstrap();
-
-// import express from 'express';
-// import { trpcRouter } from './router';
-// import { applyTrpcToExpressApp } from './lib/trpc';
-// import cors from 'cors';
-// import { AppContext, createAppContext } from './lib/ctx';
-// export type { TrpcRouter } from './router';
-
-// void (async () => {
-//   let ctx: AppContext | null = null;
-//   try {
-//     ctx = createAppContext();
-//     const expressApp = express();
-//     expressApp.use(cors());
-//     expressApp.get('/ping', (_req, res) => {
-//       res.send('pong');
-//     });
-
-//     applyTrpcToExpressApp(expressApp, ctx, trpcRouter);
-
-//     expressApp.listen(3000, () => {
-//       console.info('Listening at http://localhost:3000');
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     await ctx?.stop();
-//   }
-// })();
+ 
--- backend/src/lib/trpc.ts
+++ backend/src/lib/trpc.ts
@@ -2,29 +2,3 @@ import { initTRPC } from '@trpc/server';
 import type { AppContext } from './ctx';
 
 export const trpc = initTRPC.context<AppContext>().create();
-
-
-
-
-// import { initTRPC } from '@trpc/server';
-// import * as trpcExpress from '@trpc/server/adapters/express';
-// import { type Express } from 'express';
-// import { type TrpcRouter } from '../router';
-// import { AppContext } from './ctx';
-
-// // инициализируем
-// export const trpc = initTRPC.context<AppContext>().create();
-
-// export const applyTrpcToExpressApp = (
-//   expressApp: Express,
-//   appContext: AppContext,
-//   trpcRouter: TrpcRouter,
-// ) => {
-//   expressApp.use(
-//     '/trpc',
-//     trpcExpress.createExpressMiddleware({
-//       router: trpcRouter,
-//       createContext: () => appContext,
-//     }),
-//   );
-// };
