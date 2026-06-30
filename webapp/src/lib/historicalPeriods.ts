8107a61594f37decb5be899699abce24b220417e
Lana <grudininasv2018@gmail.com>
2026-06-30 09:54:18 +0300

    remove prisma from frontend

--- webapp/src/lib/historicalPeriods.ts
+++ webapp/src/lib/historicalPeriods.ts
@@ -1,18 +1,14 @@
 export const historicalPeriodLabels = {
   ANCIENT: 'Древний мир',
-
   MIDDLE_AGES: 'Средневековье',
-
   NAPOLEONIC_WARS: 'Наполеоновские войны',
-
   WORLD_WAR_1: 'Первая мировая война',
-
   WORLD_WAR_2: 'Вторая мировая война',
-
   FANTASY: 'Фэнтези',
-
   OTHER: 'Иное',
 } as const;
+
+export type HistoricalPeriod = keyof typeof historicalPeriodLabels;
 
 export const historicalPeriodOptions = Object.entries(
   historicalPeriodLabels,
