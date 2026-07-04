267040a1ddf935bfa003f9dbae1b77e79fb15049
Lana <grudininasv2018@gmail.com>
2026-07-04 09:40:05 +0300

    replace localhost with api config

--- webapp/src/api/uploadImage.ts
+++ webapp/src/api/uploadImage.ts
@@ -1,16 +1,15 @@
+import { API_URL } from '../lib/config';
+
 export const uploadImage = async (file: File) => {
   const formData = new FormData();
 
   formData.append('image', file);
 
-  const response = await fetch(
-    'https://miniaturenickbackend-production.up.railway.app/upload/card-image',
-    {
-      method: 'POST',
-      body: formData,
-      credentials: 'include',
-    },
-  );
+  const response = await fetch(`${API_URL}/upload/card-image`, {
+    method: 'POST',
+    body: formData,
+    credentials: 'include',
+  });
 
   if (!response.ok) {
     const errorText = await response.text();
